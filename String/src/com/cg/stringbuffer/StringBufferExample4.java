38dee93f2b28480475162e1fd08eb7c021dd7b8c
Phulare123 <103638829+Phulare123@users.noreply.github.com>
2023-02-09 17:08:43 +0530

    StringBuffer

---
+++ String/src/com/cg/stringbuffer/StringBufferExample4.java
@@ -0,0 +1,5 @@
+package com.cg.stringbuffer;
+
+public class StringBufferExample4 {
+
+}
---
+++ String/src/com/cg/stringbuffer/StringBufferexample1.java
@@ -0,0 +1,14 @@
+package com.cg.stringbuffer;
+
+public class StringBufferexample1 
+{
+	public static void main(String args[]) {
+		      StringBuffer sBuffer = new StringBuffer("test");
+		      sBuffer.append(" String Buffer");
+		      System.out.println(sBuffer);  
+		   }
+		
+
+}
+
+
